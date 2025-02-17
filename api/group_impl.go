33de89d5678210834d3a310066f1c2e5c8385fa0
Chenhao Liu <jameschenhaoliu@gmail.com>
2025-02-17 08:07:03 +0000

    implement GetAPIGroupsGroupID

--- api/group_impl.go
+++ api/group_impl.go
@@ -13,6 +13,8 @@ import (
 // Get a group by query params.
 // (GET /api/groups)
 func (s Server) GetAPIGroups(w http.ResponseWriter, r *http.Request, params GetAPIGroupsParams) {
+	return
+
 }
 
 // Get all groups for current user.
@@ -55,6 +57,7 @@ func (s Server) GetAPIGroupsMe(w http.ResponseWriter, r *http.Request) {
 				if err != nil {
 					s.Logger.Error("failed to convert groupable to group")
 					sendError(w, http.StatusInternalServerError, "Failed to convert groupable to groups")
+					return
 				}
 				groups = append(groups, group)
 			}
@@ -69,6 +72,49 @@ func (s Server) GetAPIGroupsMe(w http.ResponseWriter, r *http.Request) {
 // (GET /api/groups/{groupID})
 func (s Server) GetAPIGroupsGroupID(w http.ResponseWriter, r *http.Request, groupID uint32) {
 
+	ctx, cancel := context.WithTimeout(r.Context(), database.DatabaseTimeout)
+	defer cancel()
+
+	// Get userID from request
+	userID, ok := r.Context().Value(UserIDCtxKey{}).(uint32)
+	if !ok {
+		s.Logger.Error("failed to get userid from request context")
+		sendError(w, http.StatusUnauthorized, "Try again later.")
+		return
+	}
+
+	graph, err := CreateMSFTGraphClient(ctx, s.MSALClient, s.DB, userID)
+	if err != nil {
+		s.Logger.Error("failed to create msgraph client", zap.Error(err))
+		sendError(w, http.StatusBadGateway, "Failed to connect to microsoft graph API")
+		return
+	}
+
+	groupIDStr := strconv.FormatUint(uint64(groupID), 10)
+
+	groupable, err := graph.Groups().ByGroupId(groupIDStr).Get(ctx, nil)
+	if err != nil {
+		s.Logger.Error("failed to get group from microsoft")
+		sendError(w, http.StatusNotFound, "Failed to find group")
+		return
+	}
+
+	var group Group
+
+	if groupable != nil && groupable.GetId() != nil && groupable.GetDisplayName() != nil {
+		group, err = GroupableToGroup(groupable)
+		if err != nil {
+			s.Logger.Error("error converting groupable")
+			sendError(w, http.StatusInternalServerError, "Failed to convert groupable")
+			return
+		}
+	} else {
+		s.Logger.Error("got invalid group from microsoft")
+		sendError(w, http.StatusInternalServerError, "Invalid group")
+		return
+	}
+
+	SetHeaderAndWriteResponse(w, http.StatusOK, group)
 }
 
 // Get all members of a group.
