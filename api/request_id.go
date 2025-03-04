1cb53f44ebc83025ae018b0206dd25cd99cb69de
Andrew Fenn <afenn666@gmail.com>
2025-03-04 14:56:56 +0000

    fix: code quality

--- api/request_id.go
+++ api/request_id.go
@@ -18,7 +18,7 @@ func WriteReqUUID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
 		reqUUID := uuid.New().String()
 		ctx := context.WithValue(req.Context(), ReqUUIDKey, reqUUID)
-		writer.Header().Set(string(ReqHeader), reqUUID)
+		writer.Header().Set(ReqHeader, reqUUID)
 		next.ServeHTTP(writer, req.WithContext(ctx))
 	})
 }
@@ -26,7 +26,6 @@ func WriteReqUUID(next http.Handler) http.Handler {
 func ReadReqUUID(req *http.Request) string {
 	if reqUUID, ok := req.Context().Value(ReqUUIDKey).(string); ok {
 		return reqUUID
-	} else {
-		return ""
 	}
+	return "Error fetching ID of Request!"
 }
