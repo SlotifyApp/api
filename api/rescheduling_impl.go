aba232d5ff8cd696141afde2ba7cd30af6139190
jnile <jaidevnileshkumar2002@gmail.com>
2025-03-09 23:45:10 +0000

    Rescheduling Request functionality implemented

--- api/rescheduling_impl.go
+++ api/rescheduling_impl.go
@@ -3,10 +3,12 @@ package api
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/SlotifyApp/slotify-backend/database"
+	"github.com/avast/retry-go"
 	"go.uber.org/zap"
 )
 
@@ -92,14 +94,82 @@ func (s Server) PostAPIRescheduleRequest(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	graph, err := CreateMSFTGraphClient(ctx, s.MSALClient, s.DB, userID)
+	// graph, err := CreateMSFTGraphClient(ctx, s.MSALClient, s.DB, userID)
+	// if err != nil {
+	// 	s.Logger.Error("failed to create msgraph client", zap.Error(err))
+	// 	sendError(w, http.StatusBadGateway, "Failed to connect to microsoft graph API")
+	// 	return
+	// }
+
+	// Get data from db
+	var meeting database.Meeting
+	meeting, err = s.DB.GetMeetingByID(ctx, uint32(*body.OldMeeting.MeetingID))
+
 	if err != nil {
-		s.Logger.Error("failed to create msgraph client", zap.Error(err))
-		sendError(w, http.StatusBadGateway, "Failed to connect to microsoft graph API")
+		s.Logger.Error("failed to get data from db.Meeting", zap.Error(err))
+		sendError(w, http.StatusBadGateway, "Failed to get data from db.Meeting")
 		return
 	}
 
-	resBody, err := processReschedulingRequest(ctx, graph, body)
+	// Create Rescheduling Request
+	var requestId int64
+	err = retry.Do(func() error {
+		if requestId, err = s.DB.CreateReschedulingRequest(ctx, meeting.ID); err != nil {
+			return fmt.Errorf("Failed to create reschedling requested by user: %w", err)
+		}
+		return nil
+	}, retry.Attempts(3), retry.Delay(time.Millisecond*500))
+
+	//Create requested by time
+	params := database.CreateReschedulingRequestedByUserParams{
+		RequestID: uint32(requestId),
+		UserID:    userID,
+	}
+
+	err = retry.Do(func() error {
+		if _, err = s.DB.CreateReschedulingRequestedByUser(ctx, params); err != nil {
+			return fmt.Errorf("Failed to create reschedling requested by user: %w", err)
+		}
+		return nil
+	}, retry.Attempts(3), retry.Delay(time.Millisecond*500))
+
+	// Create Placeholder meeting info
+	placeholderParams := database.CreatePlaceholderMeetingParams{
+		RequestID:      uint32(requestId),
+		Title:          *body.NewMeeting.Title,
+		OwnerID:        userID,
+		StartTime:      *body.NewMeeting.StartTime,
+		EndTime:        *body.NewMeeting.EndTime,
+		Location:       *body.NewMeeting.Location,
+		Duration:       int32(*body.NewMeeting.Duration),
+		StartDateRange: *body.NewMeeting.StartRangeTime,
+		EndDateRange:   *body.NewMeeting.EndRangeTime,
+	}
+
+	var placeholderMeeting int64
+	err = retry.Do(func() error {
+		if placeholderMeeting, err = s.DB.CreatePlaceholderMeeting(ctx, placeholderParams); err != nil {
+			return fmt.Errorf("Failed to create placeholder meeting: %w", err)
+		}
+		return nil
+	}, retry.Attempts(3), retry.Delay(time.Millisecond*500))
+
+	// For each attendee, create placeholder attendee row
+	for _, attendee := range *body.NewMeeting.Attendees {
+		attendeeParams := database.CreatePlaceholderMeetingAttendeeParams{
+			MeetingID: int32(placeholderMeeting),
+			UserID:    uint32(len(attendee.EmailAddress.Address)), // TODO: Fix this
+		}
+
+		err = retry.Do(func() error {
+			if _, err = s.DB.CreatePlaceholderMeetingAttendee(ctx, attendeeParams); err != nil {
+				return fmt.Errorf("Failed to create placeholder meeting attendee: %w", err)
+			}
+			return nil
+		}, retry.Attempts(3), retry.Delay(time.Millisecond*500))
+	}
+
+	// NOtify user of the request
 
 	if err != nil {
 		s.Logger.Error("failed to create request", zap.Error(err))
@@ -107,5 +177,5 @@ func (s Server) PostAPIRescheduleRequest(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	SetHeaderAndWriteResponse(w, http.StatusOK, resBody)
+	SetHeaderAndWriteResponse(w, http.StatusOK, "Created")
 }
